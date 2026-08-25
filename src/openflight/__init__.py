ede2431b11b607482ec052f0be8ecd18854860f5
Dieter Olson <dieterolson@gmail.com>
2026-08-24 19:07:02 -0700

    feat(club_data): consolidate duplicated club physics and ballistics tables into unified club_data module (#18) (#38)

--- src/openflight/__init__.py
+++ src/openflight/__init__.py
@@ -2,13 +2,17 @@
 
 __version__ = "0.2.0"
 
-from .launch_monitor import ClubType, Shot, estimate_carry_distance
+from .club_data import CLUB_PROFILES, ClubProfile, ClubType, get_club_profile
+from .launch_monitor import Shot, estimate_carry_distance
 from .ops243 import Direction, OPS243Radar, SpeedReading, SpeedUnit
 
 __all__ = [
     "OPS243Radar",
     "Shot",
     "ClubType",
+    "ClubProfile",
+    "CLUB_PROFILES",
+    "get_club_profile",
     "SpeedUnit",
     "Direction",
     "SpeedReading",
